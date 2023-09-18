7f2ee74c3e7403ed653f0cc44a7c32a54e6e4570
Yuan Si <do4suki@gmail.com>
2023-09-19 04:01:27 +0800

    FlipFlap: Remove unsupported features
    Change-Id: If7c4f2809a39db41edf4ec0d53b10630f85d7604
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- src/org/lineageos/flipflap/FlipFlapView.java
+++ src/org/lineageos/flipflap/FlipFlapView.java
@@ -34,7 +34,6 @@ import android.os.BatteryManager;
 import android.os.Handler;
 import android.os.Message;
 import android.os.PowerManager;
-import android.os.Process;
 import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.UserHandle;
@@ -55,13 +54,8 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import org.lineageos.internal.util.LineageLockPatternUtils;
-import lineageos.providers.LineageSettings;
-
 public class FlipFlapView extends FrameLayout {
     private static final String TAG = "FlipFlapView";
-    private static final String KEY_PASS_TO_SECURITY = "pass_to_security_view";
-    private static final String KEY_TOUCH_SENSITIVITY = "use_high_touch_sensitivity";
 
     private static final int COVER_CLOSED_MSG = 0;
     private static final int RESTORE_SECURITY_VIEW_STATE = 1;
@@ -78,13 +72,6 @@ public class FlipFlapView extends FrameLayout {
     private boolean mAlarmActive;
     private boolean mProximityNear;
     private boolean mNotificationListenerRegistered;
-    private boolean mPassToSecurity;
-
-    private int mUserHighTouchState;
-
-    /* Required to only read the setting when it's already restored, else when closing the cover
-    within the timeout (1.5s), it would read "true" (because we set it) and always restore that */
-    private static boolean mRestoredPassToSecurity = true;
 
     public FlipFlapView(Context context) {
         super(context);
@@ -104,9 +91,6 @@ public class FlipFlapView extends FrameLayout {
 
         mWakeLock = mPowerManager.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, TAG);
         mWakeLock.setReferenceCounted(false);
-
-        changeSecurityViewState();
-        checkHighTouchSensitivity();
     }
 
     protected boolean canUseProximitySensor() {
@@ -205,8 +189,6 @@ public class FlipFlapView extends FrameLayout {
 
         mHandler.removeCallbacksAndMessages(null);
         getContext().unregisterReceiver(mReceiver);
-        restoreSecurityViewState();
-        restoreHighTouchSensitivity();
 
         if (supportsNotifications()) {
             try {
@@ -372,13 +354,6 @@ public class FlipFlapView extends FrameLayout {
         }
     }
 
-    private void restoreSecurityViewState() {
-        Message message = new Message();
-        message.what = RESTORE_SECURITY_VIEW_STATE;
-
-        mHandler.sendMessageDelayed(message, 1500);
-    }
-
     private final Handler mHandler = new Handler(true /*async*/) {
         @Override
         public void handleMessage(Message msg) {
@@ -388,63 +363,7 @@ public class FlipFlapView extends FrameLayout {
                         mPowerManager.goToSleep(SystemClock.uptimeMillis());
                     }
                     break;
-
-                case RESTORE_SECURITY_VIEW_STATE:
-                    if (shouldChangeSecurityViewState()) {
-                        setPassToSecurityView(mPassToSecurity);
-                        mPassToSecurity = false;
-                        mRestoredPassToSecurity = true;
-                    }
-                    break;
             }
         }
     };
-
-    private void changeSecurityViewState() {
-        if (shouldChangeSecurityViewState() && mRestoredPassToSecurity) {
-            mPassToSecurity = shouldPassToSecurityView();
-            setPassToSecurityView(true);
-            mRestoredPassToSecurity = false;
-        }
-    }
-
-    private boolean shouldChangeSecurityViewState() {
-        return FlipFlapUtils.getPreferences(mContext).getBoolean(KEY_PASS_TO_SECURITY, false);
-    }
-
-    private boolean shouldPassToSecurityView() {
-        LineageLockPatternUtils llpu = new LineageLockPatternUtils(mContext);
-        return llpu.shouldPassToSecurityView(getUserId());
-    }
-
-    private void setPassToSecurityView(boolean enabled) {
-        LineageLockPatternUtils llpu = new LineageLockPatternUtils(mContext);
-        llpu.setPassToSecurityView(enabled, getUserId());
-    }
-
-    private void checkHighTouchSensitivity() {
-        if (shouldUseHighTouchSensitivity() &&
-                FlipFlapUtils.getHighTouchSensitivitySupported(getContext())) {
-            mUserHighTouchState = LineageSettings.System.getInt(mContext.getContentResolver(),
-                    LineageSettings.System.HIGH_TOUCH_SENSITIVITY_ENABLE, 0);
-            LineageSettings.System.putInt(mContext.getContentResolver(),
-                    LineageSettings.System.HIGH_TOUCH_SENSITIVITY_ENABLE, 1);
-        }
-    }
-
-    private void restoreHighTouchSensitivity() {
-        if (shouldUseHighTouchSensitivity() &&
-                FlipFlapUtils.getHighTouchSensitivitySupported(getContext())) {
-            LineageSettings.System.putInt(mContext.getContentResolver(),
-                    LineageSettings.System.HIGH_TOUCH_SENSITIVITY_ENABLE, mUserHighTouchState);
-        }
-    }
-
-    private boolean shouldUseHighTouchSensitivity() {
-        return FlipFlapUtils.getPreferences(mContext).getBoolean(KEY_TOUCH_SENSITIVITY, false);
-    }
-
-    private int getUserId() {
-        return UserHandle.getUserId(Process.myUid());
-    }
 }
